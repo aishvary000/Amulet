c2906c703eb7ba8f22bc3df0b1ab191f0880c614
Soumya-24gal <soumyagoyal24@gmail.com>
2021-06-11 13:24:23 +0530

    added intent to both buttons

---
+++ app/src/main/java/eu/tutorials/evepeeve/FacultySignup.kt
@@ -0,0 +1,11 @@
+package eu.tutorials.evepeeve
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FacultySignup : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_faculty_signup)
+    }
+}
--- app/src/main/java/eu/tutorials/evepeeve/MainActivity.kt
+++ app/src/main/java/eu/tutorials/evepeeve/MainActivity.kt
@@ -1,8 +1,10 @@
 package eu.tutorials.evepeeve
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,6 +12,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         //aishvary dumbo hai
         //Gal log..
+        student_button.setOnClickListener {
+            val intent = Intent(this,StudentSignup::class.java)
+            startActivity(intent)
+            finish()
+        }
+        faculty_button.setOnClickListener {
+            val intent = Intent(this,FacultySignup::class.java)
+            startActivity(intent)
+            finish()
+        }
+
 
 
     }
