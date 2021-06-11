c0455b8a0318bf4a6798b988ba49e500e2727ee5
Soumya-24gal <soumyagoyal24@gmail.com>
2021-06-11 11:06:59 +0530

    icons added

--- app/src/main/java/eu/tutorials/evepeeve/MainActivity.kt
+++ app/src/main/java/eu/tutorials/evepeeve/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //aishvary dumbo hai
-        //Gal log
+        //Gal log..
     }
 }
