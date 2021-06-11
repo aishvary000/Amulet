3128a6e8c6c94ca96c7f1a94d0ff4f31645ed72a
Soumya-24gal <soumyagoyal24@gmail.com>
2021-06-11 17:00:27 +0530

    made signup page fancy

--- app/src/main/java/eu/tutorials/evepeeve/StudentSignup.kt
+++ app/src/main/java/eu/tutorials/evepeeve/StudentSignup.kt
@@ -2,10 +2,16 @@ package eu.tutorials.evepeeve
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_student_signup.*
+import android.widget.EditText
 
 class StudentSignup : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_student_signup)
+
+
+
     }
 }
