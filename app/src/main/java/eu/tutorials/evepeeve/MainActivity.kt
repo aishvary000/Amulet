e050f7bbffd6ed293b79f66a8ec9cab1466f2715
Soumya-24gal <soumyagoyal24@gmail.com>
2021-06-13 11:38:44 +0530

    on click listeners for textview

--- app/src/main/java/eu/tutorials/evepeeve/MainActivity.kt
+++ app/src/main/java/eu/tutorials/evepeeve/MainActivity.kt
@@ -3,6 +3,7 @@ package eu.tutorials.evepeeve
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageButton
 import kotlinx.android.synthetic.main.activity_faculty_signup.*
 import kotlinx.android.synthetic.main.activity_main.*
@@ -27,5 +28,16 @@ class MainActivity : AppCompatActivity() {
 
 
 
+    }
+    fun student_signup(v: View){
+        val intent = Intent(this, StudentSignup::class.java)
+        startActivity(intent)
+        finish()
+
+    }
+    fun perform_action(v:View){
+        val intent = Intent(this,AdminLogin::class.java)
+        startActivity(intent)
+        finish()
     }
 }
