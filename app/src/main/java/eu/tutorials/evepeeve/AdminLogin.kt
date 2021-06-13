5cf83dba49d26603a8ee5feee3ace25bd050de13
aishvary000 <19ucc070@lnmiit.ac.in>
2021-06-13 14:25:27 +0530

    bg chnge

--- app/src/main/java/eu/tutorials/evepeeve/AdminLogin.kt
+++ app/src/main/java/eu/tutorials/evepeeve/AdminLogin.kt
@@ -2,10 +2,17 @@ package eu.tutorials.evepeeve
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_admin_login.*
 
-class AdminLogin : AppCompatActivity() {
+class AdminLogin : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin_login)
+        Listeners()
+    }
+    fun Listeners()
+    {
+        validateLocally("Email",AdminLoginEmailText,AdminLoginEmailField)
+        validateLocally("Password",AdminLoginPasswordText,AdminLoginPasswordField)
     }
 }
--- app/src/main/java/eu/tutorials/evepeeve/MainActivity.kt
+++ app/src/main/java/eu/tutorials/evepeeve/MainActivity.kt
@@ -8,13 +8,20 @@ import android.widget.ImageButton
 import kotlinx.android.synthetic.main.activity_faculty_signup.*
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Listeners()
 
-        // dumbo log dumbo hi rhenge
 
+
+
+    }
+    fun Listeners()
+    {
+        validateLocally("Email",mainActivityLoginEmailText,mainActivityLoginEmailField)
+        validateLocally("Password",mainActivityLoginPasswordText,mainActivityLoginPasswordField)
     }
     fun student_signup(v: View){
         val intent = Intent(this, StudentSignup::class.java)
