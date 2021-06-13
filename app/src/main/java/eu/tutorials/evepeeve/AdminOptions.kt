ec37861096de256bfdfd77ba40c566e351f9bb86
Soumya-24gal <soumyagoyal24@gmail.com>
2021-06-13 14:32:23 +0530

    admin options

---
+++ app/src/main/java/eu/tutorials/evepeeve/AdminOptions.kt
@@ -0,0 +1,11 @@
+package eu.tutorials.evepeeve
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AdminOptions : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_admin_options)
+    }
+}
