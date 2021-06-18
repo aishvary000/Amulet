e6e4c1201f71cadf9f8bba8c9053655bafae0156
aishvary000 <19ucc070@lnmiit.ac.in>
2021-06-18 13:44:18 +0530

    final

--- app/src/main/java/eu/tutorials/evepeeve/AdminOptions.kt
+++ app/src/main/java/eu/tutorials/evepeeve/AdminOptions.kt
@@ -2,10 +2,14 @@ package eu.tutorials.evepeeve
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_admin_options.*
 
 class AdminOptions : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin_options)
+        setSupportActionBar(AdminOptionsAppbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
     }
 }
