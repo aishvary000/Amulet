db237536e79a91390b07a2670a1c5786fe6b2cba
aishvary000 <54467728+aishvary000@users.noreply.github.com>
2021-06-24 11:42:36 +0530

    final

--- app/src/main/java/eu/tutorials/evepeeve/ClubAdminEditInfo.kt
+++ app/src/main/java/eu/tutorials/evepeeve/ClubAdminEditInfo.kt
@@ -2,14 +2,17 @@ package eu.tutorials.evepeeve
 
 import android.app.Dialog
 import android.content.Intent
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.Toast
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.type.TimeOfDay
 import eu.tutorials.evepeeve.Database.DatabaseManagement
+import eu.tutorials.evepeeve.Models.Clubs
 import kotlinx.android.synthetic.main.activity_club_admin_edit_info.*
 import kotlinx.android.synthetic.main.custom_progress_bar.*
 import kotlinx.android.synthetic.main.custom_toast.*
@@ -26,21 +29,45 @@ class ClubAdminEditInfo : AppCompatActivity() {
         setContentView(R.layout.activity_club_admin_edit_info)
         newAdminSaveChanges.setOnClickListener {
             if(id!=null) {
-                if(newAdminNameText.text.toString() == "" || newAdminEmailText.text.toString() == "")
+                if(newAdminEmailText.text.toString() == "")
                 {
                     Toast.makeText(this,"Both Fileds Are required",Toast.LENGTH_SHORT).show()
                 }
                 else
                 {
                     showProgressDialog("Update in progress")
-                    DatabaseManagement().updateAdminClubInfo(this,id,newAdminNameText.text.toString(),newAdminEmailText.text.toString(),this)
-
+                    DatabaseManagement().lookForEmail(newAdminEmailText.text.toString(),this,id)
                 }
 
             }
         }
 
 
+    }
+    fun userFoundorNot(info:String,docId:String){
+
+       if(info == "") //user not found
+       {
+           //register in db with designation
+           mDialog?.dismiss()
+           Toast.makeText(this,"User Not Found try registering First",Toast.LENGTH_LONG).show()
+           if(newAdminNameVisibilty.visibility != View.VISIBLE)
+                newAdminNameVisibilty.visibility = View.VISIBLE
+           if(clubNameVisibilty.visibility != View.VISIBLE)
+               clubNameVisibilty.visibility = View.VISIBLE
+           if(paswordFieldVisibilty.visibility != View.VISIBLE)
+                paswordFieldVisibilty.visibility = View.VISIBLE
+           //setting clubName according to the docId captured
+           FirebaseFirestore.getInstance().collection("Clubs").document(docId).get()
+               .addOnSuccessListener {
+                   val item:Clubs? = it.toObject(Clubs::class.java)
+                   if (item != null) {
+                       clubNameText.setText(item.clubName)
+                       clubNameText.isEnabled = false
+                   }
+               }
+
+       }
     }
     fun showProgressDialog(text:String){
         mDialog= Dialog(this)
@@ -56,12 +83,14 @@ class ClubAdminEditInfo : AppCompatActivity() {
 
     }
     fun showError(messgae:String){
-        FailureToast(messgae)
         mDialog?.dismiss()
+        FailureToast(messgae)
+
 
     }
     fun FailureToast(message:String)
     {
+        mDialog?.dismiss()
         var inflater: LayoutInflater = LayoutInflater.from(this)
         var layout: View = inflater.inflate(R.layout.custom_toast_error,customllError)
         layout.custom_Toast_text_Failure.text = message
--- app/src/main/java/eu/tutorials/evepeeve/Database/DatabaseManagement.kt
+++ app/src/main/java/eu/tutorials/evepeeve/Database/DatabaseManagement.kt
@@ -1,4 +1,5 @@
 package eu.tutorials.evepeeve.Database
+import android.app.Activity
 import android.content.Context
 import android.util.Log
 import android.widget.Toast
@@ -16,7 +17,9 @@ class DatabaseManagement:BaseActivity() {
 
     private val firestore = FirebaseFirestore.getInstance()
     private val firebaseAuth = FirebaseAuth.getInstance()
-    private var userId:String = ""
+    private val fireStoreUser: CollectionReference = firestore.collection("Users")
+    private val fireStoreClubs: CollectionReference = firestore.collection("Clubs")
+    private var userId: String = ""
     fun registerStudentInFirestore(userInfo: Users, activity: StudentSignup) {
         firestore.collection(eu.tutorials.evepeeve.utils.Constants.studentsDb).document(userInfo.id)
             .set(userInfo, SetOptions.merge())
@@ -28,12 +31,15 @@ class DatabaseManagement:BaseActivity() {
 
             }
     }
+
     fun getClubs(): FirestoreRecyclerOptions<Clubs> {
-        var clubsReference:CollectionReference = firestore.collection("Clubs")
-        return FirestoreRecyclerOptions.Builder<Clubs>().setQuery(clubsReference,Clubs::class.java).build()
+        var clubsReference: CollectionReference = firestore.collection("Clubs")
+        return FirestoreRecyclerOptions.Builder<Clubs>().setQuery(clubsReference, Clubs::class.java)
+            .build()
 
     }
-    fun registerStudentForAuthorization(userInfo: Users, activity: StudentSignup){
+
+    fun registerStudentForAuthorization(userInfo: Users, activity: StudentSignup) {
         firebaseAuth.createUserWithEmailAndPassword(userInfo.email, userInfo.password)
             .addOnSuccessListener { it ->
                 //updating id of user to that of firebase
@@ -42,7 +48,7 @@ class DatabaseManagement:BaseActivity() {
                     userInfo.id = firebase.uid
                 }
                 //registering for firebase
-                registerStudentInFirestore(userInfo,activity)
+                registerStudentInFirestore(userInfo, activity)
 
 
             }
@@ -50,34 +56,35 @@ class DatabaseManagement:BaseActivity() {
                 Log.e("this", it.toString())
             }
     }
-    fun loginAdmin(email:String,password:String,context:Context,activity:MainActivity)
-    {
 
-        firebaseAuth.signInWithEmailAndPassword(email,password)
-            .addOnSuccessListener {it->
+    fun loginAdmin(email: String, password: String, context: Context, activity: MainActivity) {
 
-                    firestore.collection("Users").document(getCurrentUserId()).get()
-                        .addOnSuccessListener {it->
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+            .addOnSuccessListener { it ->
 
-                           activity.signInUser(it,context,activity)
+                firestore.collection("Users").document(getCurrentUserId()).get()
+                    .addOnSuccessListener { it ->
 
-                        }
-                        .addOnFailureListener {
+                        activity.signInUser(it, context, activity)
 
-                            activity.showError("User Not in Databaase")
+                    }
+                    .addOnFailureListener {
 
-                            Log.e("here",it.toString())
-                        }
+                        activity.showError("User Not in Databaase")
+
+                        Log.e("here", it.toString())
+                    }
             }
             .addOnFailureListener {
 
                 activity.showError("User Not found")
-                Log.e("error : ",it.toString())
+                Log.e("error : ", it.toString())
             }
 
 
     }
-    fun registerAdminForAuthorization(userInfo: Users, activity: SignUpByAdmin){
+
+    fun registerAdminForAuthorization(userInfo: Users, activity: SignUpByAdmin) {
         firebaseAuth.createUserWithEmailAndPassword(userInfo.email, userInfo.password)
             .addOnSuccessListener { it ->
                 //updating id of user to that of firebase
@@ -86,7 +93,7 @@ class DatabaseManagement:BaseActivity() {
                     userInfo.id = firebase.uid
                 }
                 //registering for firebase
-                registerAdminInFirestore(userInfo,activity)
+                registerAdminInFirestore(userInfo, activity)
 
 
             }
@@ -94,6 +101,7 @@ class DatabaseManagement:BaseActivity() {
                 Log.e("this", it.toString())
             }
     }
+
     fun registerAdminInFirestore(userInfo: Users, activity: SignUpByAdmin) {
         firestore.collection(eu.tutorials.evepeeve.utils.Constants.studentsDb).document(userInfo.id)
             .set(userInfo, SetOptions.merge())
@@ -105,13 +113,20 @@ class DatabaseManagement:BaseActivity() {
 
             }
     }
-    fun updateAdminClubInfo(context: Context,id:String,newAdminName:String,newAdminEmail:String,activity:ClubAdminEditInfo){
-        var docRefClubs:CollectionReference = firestore.collection("Clubs")
-        var clubName:String = ""
+
+    fun updateAdminClubInfo(
+        context: Context,
+        id: String,
+        newAdminName: String,
+        newAdminEmail: String,
+        activity: ClubAdminEditInfo
+    ) {
+        var docRefClubs: CollectionReference = firestore.collection("Clubs")
+        var clubName: String = ""
         firestore.collection("users").document(id).delete()
         docRefClubs.document(id).get()
             .addOnSuccessListener {
-                if(it!=null) {
+                if (it != null) {
                     val doc = it.toObject(Clubs::class.java)
                     if (doc != null) {
                         clubName = doc.clubName
@@ -120,8 +135,8 @@ class DatabaseManagement:BaseActivity() {
             }
 
         //checking for any document with email id as new email id
-        var collectionReferenceUsers:CollectionReference = firestore.collection("Users")
-        collectionReferenceUsers.whereEqualTo("email",newAdminEmail)
+        var collectionReferenceUsers: CollectionReference = firestore.collection("Users")
+        collectionReferenceUsers.whereEqualTo("email", newAdminEmail)
             .get()
             .addOnSuccessListener { documents ->
                 if (documents != null) {
@@ -129,7 +144,7 @@ class DatabaseManagement:BaseActivity() {
                         userId = document.id
                     }
                     if (userId == "") {
-                        Log.e("This user not found","User Not")
+                        Log.e("This user not found", "User Not")
                         firebaseAuth.createUserWithEmailAndPassword(newAdminEmail, "qwertyuiop")
                             .addOnSuccessListener {
                                 var user: Users = Users(
@@ -168,9 +183,7 @@ class DatabaseManagement:BaseActivity() {
                             }
 
 
-                    }
-                    else
-                    {
+                    } else {
                         Log.e("What ", userId)
                         var map: HashMap<String, Any> = HashMap()
                         map["designation"] = "clubAdmin"
@@ -203,20 +216,196 @@ class DatabaseManagement:BaseActivity() {
                 }
             }
             .addOnFailureListener {
-                                activity.showError(it.toString())
+                activity.showError(it.toString())
             }
+
+
+    }
+
+    fun createUserwithPriviledges(
+        name: String,
+        email: String,
+        password: String,
+        Role: String,
+        adminOf: String,
+        activity: ClubAdminEditInfo
+    ) {
+        var Userid: String = ""
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+            .addOnSuccessListener {
+                var user: Users = Users(
+                    name = name,
+                    email = email,
+                    designation = Role
+                )
+                val firebase: FirebaseUser? = it.user
+                if (firebase != null) {
+                    Log.e("uid ", firebase.uid)
+                    user.id = firebase.uid
+
+                }
+
+                //registering in users collection
+                firestore.collection("Users").document(user.id).set(user)
+                    .addOnSuccessListener {
+                        Userid = user.id
+                        Log.e("this ", "New User Registration Successful $userId")
+                        Log.e("Here ", userId)
+                        var docRef: CollectionReference
+                        if (Role == "clubAdmin") {
+                            docRef = firestore.collection("Clubs")
+                            docRef.document(userId).set(
+                                Clubs(
+                                    clubName = adminOf,
+                                    name = name,
+                                    email = email,
+                                    uid = Userid
+                                )
+                            ).addOnSuccessListener {
+
+                                activity.showSucessToast("Club Admin Updated")
+                            }
+                                .addOnFailureListener {
+                                    activity.showError("Failed")
+                                }
+
+                        }
+
+                    }
+            }
+    }
+
+    fun lookForEmail(email: String,activity: ClubAdminEditInfo,docId:String){
+            var document:String = ""
+            fireStoreUser.whereEqualTo("email",email).get().addOnSuccessListener { documents->
+                if(documents.isEmpty)
+                {
+
+                        Log.e("This ","User Not found")
+
+                        activity.userFoundorNot(document,docId)
+
+
+                    }
+                }
+                Log.e("Not working","Here")
+            }
+
 
 
     }
 
 
 
+//
+//    fun searchUserandTryRegistering(docClubTodeleteId:String,name:String,email: String,activity: ClubAdminEditInfo,Role:String){
+//        var docId:String = ""
+//        //getting user in Users
+//        var collectionReferenceUsers: CollectionReference = firestore.collection("Users")
+//        collectionReferenceUsers.whereEqualTo("email", email)
+//            .get()
+//            .addOnSuccessListener { documents ->
+//                if (documents != null) {
+//
+//                    for(document in documents) {
+//                        docId = document.id
+//                    }
+//                        Log.e("What ", docId)
+//                    //getting club snapshot
+//                    var UserInfo:Users? = null
+//                    var clubInfo:Clubs? = null
+//                        firestore.collection("Users").document(docId).get().addOnSuccessListener {
+//                            if(it != null)
+//                                UserInfo = it.toObject(Users::class.java)
+//                        }
+//                        if(UserInfo?.designation == "Faculty") {
+//                            Log.e("This is a faculty Id", UserInfo!!.id)
+//                            activity.showError("Id is of faculty")
+//
+//                        }
+//                        var map: HashMap<String, Any> = HashMap()
+//                        map["designation"] = Role
+//
+//                        var docRefClubs: CollectionReference = firestore.collection("Clubs")
+//                        var clubName:String = ""
+//
+//                        docRefClubs.document(docClubTodeleteId).get()
+//                            .addOnSuccessListener {
+//                                if(it!=null) {
+//                                    clubInfo = it.toObject(Clubs::class.java)
+//                                    if (clubInfo != null) {
+//                                        clubName = clubInfo!!.clubName
+//
+//                                    }
+//                                }
+//                            }
+//                            .addOnFailureListener {
+//                                Log.e("Falied ","To get club info")
+//                                activity.showError("Club Not found")
+//                            }
+//                        docRefClubs.document(docClubTodeleteId).set(
+//                            Clubs(
+//
+//                                                name = name,
+//                                                email = email,
+//                                                uid = docId
+//                                            )
+//                                        ).addOnSuccessListener {
+//                                            docRefClubs.document(docClubTodeleteId).get().addOnSuccessListener {
+//                                                if(it!=null)
+//                                                {
+//
+//
+//                                                        docRefClubs.document(docClubTodeleteId).delete()
+//                                                        //deleting users from users
+//                                                        firestore.collection("Users").document(clubItem!!.uid).update(map)
+//                                                            .addOnFailureListener {
+//                                                                Log.e("Here ", it.toString())
+//
+//
+//                                                    }
+//                                                        activity.showSucessToast("Updated")
+//
+//                                                }
+//
+//
+//
+//                                            }
+//
+//
+//
+//                                        }
+//                                            .addOnFailureListener {
+//                                                activity.showError("Failed")
+//                                            }
+//                                    }
+//
+//
+//                            }
+//
+//
+//                else{
+//                    activity.FailureToast("User not found Try adding first")}
+//
+//
+//            }
+//
+//
+//                }
+//
+//
+//    }
 
 
 
 
 
-}
+
+
+
+
+
+
 
 
 
